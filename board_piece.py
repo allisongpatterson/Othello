35702c71e6dc6decc946fc77ee2b3e27b9f45221
Hayley Hansson <user@Hayley-Hanssons-MacBook-Pro.local>
2013-12-12 15:00:03 -0500

    working

--- board_piece.py
+++ board_piece.py
@@ -40,14 +40,12 @@ class Gameplay(object):
 
     def __init__(self,board,player,space):
         self.board = Board()
-        self.player = Piece('B')
+        self.player = Piece('W')
         self.space = space
 
-    def __str__(self):
-        return str(self.board)
 
-    def diag_pos(self,player,space,board):
-        '''checks diagonal positive positive'''
+    def direct1(self,player,space,board):
+        '''checks diagonal positive positive: x+1, y+1'''
         if self.player == 'B':
             oppcolor = 'W'
         else:
@@ -69,7 +67,77 @@ class Gameplay(object):
             if value == 'C':
                 value = oppcolor
 
-    
+
+    def direct2(self,player,space,board):
+        '''checks directly above: x+0, y+1'''
+        if self.player == 'B':
+            oppcolor = 'W'
+        else:
+            oppcolor = 'B'
+        for i in range(1,8):
+            square1 = (self.space[1],self.space[2]+i)
+            if self.board[square1] == 'E':
+                break
+            if self.board[square1] == oppcolor:
+                self.board[square1] = 'C'
+                i+1
+            if self.board[square1] == self.player:
+                if self.board[(self.space[1],self.space[2]+(i-1))] == oppcolor:
+                    if value == 'C':
+                        value = self.player
+                else:
+                    break
+        for value in self.board:
+            if value == 'C':
+                value = oppcolor    
+
+
+    def direct3(self,player,space,board):
+        '''checks diagonal negative positive: x-1, y+1'''
+        if self.player == 'B':
+            oppcolor = 'W'
+        else:
+            oppcolor = 'B'
+        for i in range(1,8):
+            square1 = (self.space[1]-i,self.space[2]+i)
+            if self.board[square1] == 'E':
+                break
+            if self.board[square1] == oppcolor:
+                self.board[square1] = 'C'
+                i+1
+            if self.board[square1] == self.player:
+                if self.board[(self.space[1]-(i-1),self.space[2]+(i-1))] == oppcolor:
+                    if value == 'C':
+                        value = self.player
+                else:
+                    break
+        for value in self.board:
+            if value == 'C':
+                value = oppcolor 
+
+
+    # def direct4(self,player,space,board):
+    #     '''checks directly above: x+0, y+1'''
+    #     if self.player == 'B':
+    #         oppcolor = 'W'
+    #     else:
+    #         oppcolor = 'B'
+    #     for i in range(1,8):
+    #         square1 = (self.space[1],self.space[2]+i)
+    #         if self.board[square1] == 'E':
+    #             break
+    #         if self.board[square1] == oppcolor:
+    #             self.board[square1] = 'C'
+    #             i+1
+    #         if self.board[square1] == self.player:
+    #             if self.board[(self.space[1],self.space[2]+(i-1))] == oppcolor:
+    #                 if value == 'C':
+    #                     value = self.player
+    #             else:
+    #                 break
+    #     for value in self.board:
+    #         if value == 'C':
+    #             value = oppcolor 
 
     def empty_space(self,space):
         if self.board[space[1],space[2]] == 'E':
@@ -77,6 +145,8 @@ class Gameplay(object):
         else:
             return 'Sorry, that is not a valid move! Please select an empty space.'
 
+    def __str__(self):
+        return str(self.board)
 
 if __name__ == "__main__":
 
@@ -87,7 +157,7 @@ if __name__ == "__main__":
     # test_board = Board()
     # print test_board
 
-    test_game = Gameplay(Board(),'B', (3,2))
+    test_game = Gameplay(Board(),'W', (3,2))
     print test_game
 
 
