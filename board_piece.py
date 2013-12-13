541045e95930678206f2288684c1af403a72d2f2
Hayley Hansson <user@Hayley-Hanssons-MacBook-Pro.local>
2013-12-13 13:44:50 -0500

    working

--- board_piece.py
+++ board_piece.py
@@ -116,28 +116,28 @@ class Gameplay(object):
                 value = oppcolor 
 
 
-    # def direct4(self,player,space,board):
-    #     '''checks directly above: x+0, y+1'''
-    #     if self.player == 'B':
-    #         oppcolor = 'W'
-    #     else:
-    #         oppcolor = 'B'
-    #     for i in range(1,8):
-    #         square1 = (self.space[1],self.space[2]+i)
-    #         if self.board[square1] == 'E':
-    #             break
-    #         if self.board[square1] == oppcolor:
-    #             self.board[square1] = 'C'
-    #             i+1
-    #         if self.board[square1] == self.player:
-    #             if self.board[(self.space[1],self.space[2]+(i-1))] == oppcolor:
-    #                 if value == 'C':
-    #                     value = self.player
-    #             else:
-    #                 break
-    #     for value in self.board:
-    #         if value == 'C':
-    #             value = oppcolor 
+    def direct4(self,player,space,board):
+        '''checks left: x+0, y+1'''
+        if self.player == 'B':
+            oppcolor = 'W'
+        else:
+            oppcolor = 'B'
+        for i in range(1,8):
+            square1 = (self.space[1],self.space[2]+i)
+            if self.board[square1] == 'E':
+                break
+            if self.board[square1] == oppcolor:
+                self.board[square1] = 'C'
+                i+1
+            if self.board[square1] == self.player:
+                if self.board[(self.space[1],self.space[2]+(i-1))] == oppcolor:
+                    if value == 'C':
+                        value = self.player
+                else:
+                    break
+        for value in self.board:
+            if value == 'C':
+                value = oppcolor 
 
     def empty_space(self,space):
         if self.board[space[1],space[2]] == 'E':
@@ -157,7 +157,7 @@ if __name__ == "__main__":
     # test_board = Board()
     # print test_board
 
-    test_game = Gameplay(Board(),'W', (3,2))
+    test_game = Gameplay(Board(),'W', (3,4))
     print test_game
 
 
