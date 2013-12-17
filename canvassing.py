0085a97d013df328550e3a9805c7b7221e6d7087
Julie Barron <jbarron@wellesley.edu>
2013-12-17 01:43:18 -0500

    updates

--- canvassing.py
+++ canvassing.py
@@ -1,6 +1,8 @@
 from Tkinter import*
 from board_piece_final import*
 
+
+
 class Board(Canvas):
     global scaling_factor
     scaling_factor=70
@@ -29,39 +31,40 @@ class Board(Canvas):
 
 
     def makeBoard(self):
+        logic = Gameplay()
         #horizontal lines
         for n in range(1,9):
             self.create_line(0,scaling_factor*n,self.w,scaling_factor*n, fill = 'black', width=5)
         #vertical lines
         for n in range(1,9):
             self.create_line(scaling_factor*n,0,scaling_factor*n,self.w, fill = 'black',width=5)
-        #self.r=scaling_factor*.75
         self.r=scaling_factor/4
-        #print boarddict[0]
-        #self.create_oval(3*scaling_factor+self.r,4*scaling_factor+self.r,4*scaling_factor-self.r,5*scaling_factor-self.r,fill=logic.board[('3','4')],outline=logic.board[('3','4')])
-        #self.create_oval(3*scaling_factor+self.r,4*scaling_factor+self.r,4*scaling_factor-self.r,5*scaling_factor-self.r,fill=logic.board[('3','4')],outline=logic.board[('3','4')])
-
-
+        for coordinates in logic.board:
+            if logic.board[str(coordinates[0]),str(coordinates[1])] == 'white' or logic.board[str(coordinates[0]),str(coordinates[1])] == 'black':
+                self.create_oval(int(coordinates[0])*scaling_factor+self.r,int(coordinates[1])*scaling_factor+self.r,(int(coordinates[0])+1)*scaling_factor-self.r,(int(coordinates[1])+1)*scaling_factor-self.r,fill=logic.board[coordinates],outline=logic.board[coordinates])
+            # else:
+            #     return 'This is not a valid move'
+        
     def Move(self, event, point = None):
         #print "frame coordinates: %s,%s" % (event.x, event.y)
         if (event.x,event.y)<=(self.w,self.h):
             x=(int(event.x/scaling_factor)*scaling_factor)+(scaling_factor/2)
             y=(int(event.y/scaling_factor)*scaling_factor)+(scaling_factor/2)
-            self.create_oval(x-self.r,y-self.r,x+self.r,y+self.r,fill='black',outline='black')
+            #self.create_oval(x-self.r,y-self.r,x+self.r,y+self.r,fill='black',outline='black')
             #print(x-self.r,y-self.r,x+self.r,y+self.r)
             self.point = (int(event.x/scaling_factor)),(int(event.y/scaling_factor))    #returns coordinate that goes into logic code (maybe don't return this, but make it an input into code and output from code should be input of circle attributes)
         else:
             return
+        
+
 
 def main():
-    logic = Gameplay()
-    boarddict=logic.board
-    print boarddict
+    #logic = Gameplay()
+    #boarddict=logic.board
+    #print board
     root = Tk()
     Board(root)
     root.mainloop()
-    
-    #print logic.board[('3','4')]
 
 
 if __name__ == '__main__':
