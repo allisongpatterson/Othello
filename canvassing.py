deb242a587dce2f382e8e577a67901a0f1314559
AmbikaGoel <agoel@agoel-VirtualBox.(none)>
2013-12-20 23:29:12 -0500

    adding docstrings

--- canvassing.py
+++ canvassing.py
@@ -1,14 +1,19 @@
 from Tkinter import*
 from board_piece_final import*
 
-
-
 class Board(Canvas):
+    """Makes an 8 by 8 grid. Users can click anywhere on grid to place a 
+    new piece. Uses a dictionary to determine what color the circles 
+    should be.
+
+    Gameplay(): Imported class that returns a dicitonary."""
+
     global scaling_factor
 
     scaling_factor=97
 
     def __init__(self, parent):
+
         Canvas.__init__(self, parent, background='forest green')    
         
         self.parent = parent
@@ -19,6 +24,8 @@ class Board(Canvas):
         self.bind("<1>", self.Move)        
 
     def centerWindow(self):
+        """Centers the window. Makes a button that starts the game"""
+
         self.w = scaling_factor*8
         self.h = scaling_factor*8
 
@@ -31,8 +38,9 @@ class Board(Canvas):
         button=Button(self, text='Start',font="Helvetica 40",highlightbackground='forest green',background='forest green',bd=5,command=self.makeBoard)
         button.place(x=11*scaling_factor, y=3.5*scaling_factor)
     def makeBoard(self):
+        """Makes a circle on the grid. Coordinate specified by dictionary key
+        and color specified by dictionary value."""
          logic = self.game
-         #print logic.board
          #horizontal lines
          for n in range(1,9):
              self.create_line(0,scaling_factor*n,self.w,scaling_factor*n, fill = 'black', width=5)
@@ -45,29 +53,16 @@ class Board(Canvas):
                  self.create_oval(int(coordinates[0])*scaling_factor+self.r,int(coordinates[1])*scaling_factor+self.r,(int(coordinates[0])+1)*scaling_factor-self.r,(int(coordinates[1])+1)*scaling_factor-self.r,fill=logic.board[coordinates],outline=logic.board[coordinates])
  
     def Move(self, event, point = None):
-        #print "frame coordinates: %s,%s" % (event.x, event.y)
+        """Scales coordinates from user's click for Gameplay."""
         if (event.x,event.y)<=(self.w,self.h):
             x=(int(event.x/scaling_factor)*scaling_factor)+(scaling_factor/2)
             y=(int(event.y/scaling_factor)*scaling_factor)+(scaling_factor/2)
-
-            #self.create_oval(x-self.r,y-self.r,x+self.r,y+self.r,fill='black',outline='black')
-            #print(x-self.r,y-self.r,x+self.r,y+self.r)
-            #print (str(int(event.x/scaling_factor)),str(int(event.y/scaling_factor)))    #returns coordinate that goes into logic code (maybe don't return this, but make it an input into code and output from code should be input of circle attributes)
-
-            #self.create_oval(x-self.r,y-self.r,x+self.r,y+self.r,fill='black',outline='black')
-            #print(x-self.r,y-self.r,x+self.r,y+self.r)
-            self.point = (int(event.x/scaling_factor)),(int(event.y/scaling_factor))    #returns coordinate that goes into logic code (maybe don't return this, but make it an input into code and output from code should be input of circle attributes)
-            
-            #print self.point
+            self.point = (int(event.x/scaling_factor)),(int(event.y/scaling_factor))
             self.game.updateBoard(self.point)
-            #self.makeBoard()
+  
 
 def main():
     logic = Gameplay()
-    #boarddict=logic.board
-    #print board
-    #root = Tk()
-    #Board(root)
-    #root.mainloop()
+
 if __name__ == '__main__':
     main() 
